4a0f6ae053dfcf309796a534f074a7c8792942bb
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2023-10-30 20:24:46 -0300

    Cambiado el cálculo de movimiento de la cámara
    Ahora no se modifica el suavizado según el movimiento del mouse, sino la sensibilidad. Ahora la sensibilidad se reduce si el movimiento del mouse es ínfimo y se dirige a su valor verdadero cuando los ejes del input del mouse alcanzan sus valores máximos.

--- Assets/Jugador/CameraController.cs
+++ Assets/Jugador/CameraController.cs
@@ -9,9 +9,7 @@ public class CameraController : MonoBehaviour
     private float sensibilidad;
 
     [SerializeField]
-    float minSuavizado = 0.08f;
-    [SerializeField]
-    float maxSuavizado;
+    [Range(0f, 0.3f)]float suavizado;
 
     Vector2 suavidadV;
 
@@ -37,9 +35,17 @@ public class CameraController : MonoBehaviour
         var movimiento = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         // (1f - Mathf.Abs(movimiento.x)
-
+        /*
         suavidadV.x = Mathf.SmoothStep(suavidadV.x, movimiento.x * sensibilidad, Mathf.Clamp(maxSuavizado * (1f - Mathf.Abs(movimiento.x)), minSuavizado, maxSuavizado));
         suavidadV.y = Mathf.SmoothStep(suavidadV.y, movimiento.y * sensibilidad, Mathf.Clamp(maxSuavizado * (1f - Mathf.Abs(movimiento.y)), minSuavizado, maxSuavizado));
+
+        */
+
+        float sens_x = sensibilidad * Mathf.Clamp(Mathf.Abs((Mathf.Abs(movimiento.x) - 1f)), 0, 1);
+        float sens_y = sensibilidad * Mathf.Clamp(Mathf.Abs((Mathf.Abs(movimiento.y) - 1f)), 0, 1);
+
+        suavidadV.x = Mathf.SmoothStep(suavidadV.x, movimiento.x * sens_x, suavizado);
+        suavidadV.y = Mathf.SmoothStep(suavidadV.y, movimiento.y * sens_y, suavizado);
 
         // Rotamos al jugador ho1rizontalmente
         jugador.transform.Rotate(Vector3.up, suavidadV.x);

7aed17b2cdf162afb8f0a4610e5e3dc1c65ddd8f
CelestePapalato <50720541+CelestePapalato@users.noreply.github.com>
2023-10-30 21:59:30 -0300

    Update CameraController.cs

--- Assets/Jugador/CameraController.cs
+++ Assets/Jugador/CameraController.cs
@@ -34,20 +34,10 @@ public class CameraController : MonoBehaviour
 
         var movimiento = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        // (1f - Mathf.Abs(movimiento.x)
-        /*
-        suavidadV.x = Mathf.SmoothStep(suavidadV.x, movimiento.x * sensibilidad, Mathf.Clamp(maxSuavizado * (1f - Mathf.Abs(movimiento.x)), minSuavizado, maxSuavizado));
-        suavidadV.y = Mathf.SmoothStep(suavidadV.y, movimiento.y * sensibilidad, Mathf.Clamp(maxSuavizado * (1f - Mathf.Abs(movimiento.y)), minSuavizado, maxSuavizado));
+        suavidadV.x = Mathf.SmoothStep(suavidadV.x, movimiento.x * sensibilidad, suavizado);
+        suavidadV.y = Mathf.SmoothStep(suavidadV.y, movimiento.y * sensibilidad, suavizado);
 
-        */
-
-        float sens_x = sensibilidad * Mathf.Clamp(Mathf.Abs((Mathf.Abs(movimiento.x) - 1f)), 0, 1);
-        float sens_y = sensibilidad * Mathf.Clamp(Mathf.Abs((Mathf.Abs(movimiento.y) - 1f)), 0, 1);
-
-        suavidadV.x = Mathf.SmoothStep(suavidadV.x, movimiento.x * sens_x, suavizado);
-        suavidadV.y = Mathf.SmoothStep(suavidadV.y, movimiento.y * sens_y, suavizado);
-
-        // Rotamos al jugador ho1rizontalmente
+        // Rotamos al jugador horizontalmente
         jugador.transform.Rotate(Vector3.up, suavidadV.x);
 
         // Rotamos verticalmente la cámara
